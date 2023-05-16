132f7a8e85a4b2eb59e8cc0e54576be6f193d1dd
fperez15 <francis.perez1590@gmail.com>
2023-05-16 14:59:27 -0500

    creando la funcion mdLink

--- api.js
+++ api.js
@@ -3,27 +3,6 @@ import fs from "fs"; // api callback --> fs.stat(ruta, callback)
 //const fsp = fs.promise  -->fsp.stat(ruta) --> returna promesa
 import path from "path";
 
-// read the path
-// readFile('test/prueba.md', 'utf8', (error, data) => {
-//   if (error) {
-//     console.error(error);
-//   } else {
-//     console.log(data);
-//   }
-// });
-
-// validate path
-// const validatePath = 'test/prueba.md';
-
-// access(validatePath, (error) => {
-//   if (error) {
-//     console.error(` invalid path`);
-//   } else {
-//     console.log(`validate path`);
-//   }
-// });
-
-//--------------------------------------------------------------------------------------
 
 // exist path
 export const existPath = (path) => fs.existsSync(path);
@@ -48,9 +27,9 @@ export const directoryPath = (path) => {
 export const readFiles = (path) =>
   fs.readdir(path, (err, files) => {
     if (err) {
-      return console.log('error al leer carpetas',err);
+      return console.log("error al leer carpetas", err);
     }
-    files.forEach((file) => console.log("Archivos en carpeta: ", file));
+    console.log(files); 
   });
 
 // is .md file? VALIDACION PARA USAR EN LA FUNCION mdLinks (si hay archivos .md?)
@@ -68,53 +47,11 @@ export const readFiles = (path) =>
 
 // read .md file
 export const readFileMd = (path) =>
-  fs.readFile(path, 'utf8',(err, fileMd) => {
+  fs.readFile(path, "utf8", (err, fileMd) => {
     if (err) {
-          console.error('error en leer archivo.md', err);
-        } else {
-          console.log('Leer archivo.md', fileMd);
-        }
+      console.error("error en leer archivo.md", err);
+    } else {
+      console.log("Leer archivo.md", fileMd);
+    }
   });
 
-// if (stats.isDirectory()) {
-//     console.log(path, "es directorio");
-
-//   } else {
-//     console.log(path, "no es un directorio");
-
-//   }
-
-// export const mdLinks = (router, options) => {
-//   const existRouter = existPath(router);
-//   if (existRouter) {
-//     const absoluteRouter = absolutePath(router);
-
-//     if (absoluteRouter === false) {
-//       //convertir a absoluta
-//       const convertRouter = convertPath(router);
-//     }
-//     //preguntar si es directorio
-//     directoryPath(router, (isDirectory) => {
-
-//       if (isDirectory) {
-
-//       }
-
-//     });
-
-//     directoryPath(router) //promesas
-//       .then((isDirectory) => {
-//         if (isDirectory) {
-//         }
-//       });
-
-/*
-    if (absoluteRouter){
-        preguntar si es directorio
-    }else {
-        convertir a absoluta
-        preguntar si es directorio
-    }
-    */
-//   }
-// };
--- index.js
+++ index.js
@@ -5,22 +5,72 @@ const [ , , path] = process.argv;
 console.log(existPath(path));
 
 // la ruta es absoluta
-console.log(absolutePath(path));
+// console.log(absolutePath(path));
 
 //convertir a ruta absoluta
-console.log(convertPath(path));
+// console.log(convertPath(path));
 
 //saber si la ruta es un directorio
-directoryPath(path)
-  .then((esDir) => console.log(`${esDir}`))
-  .catch((error) => console.error(`${error}`));
+// directoryPath(path)
+//   .then((esDir) => console.log(`${esDir}`))
+//   .catch((error) => console.error(`${error}`));
 
 // read files
-readFiles(path);
+// readFiles(path);
 
 // .md files
 // mdFiles(path)
 
 // read .md file
-readFileMd(path);
+// readFileMd(path);
+
+export const mdLinks = (router, options) => {
+  return new Promise((resolve, reject) => {
+    const existRouter = existPath(router);
+  if (existRouter) {
+    const absoluteRouter = absolutePath(router);
+    
+    let routerConvert; 
+    if (absoluteRouter === false) {
+      //convertir a absoluta
+      routerConvert = convertPath(router);
+    } else {
+      routerConvert = router;
+    }
+    
+    //preguntar si es directorio
+    directoryPath(routerConvert)
+      .then((isDirectory) => {
+        console.log('es directorio', isDirectory);
+        if (isDirectory) {
+          
+          const readDirectory = readFiles(routerConvert);
+          console.log('leyendo directorio', readDirectory);
+          readDirectory.forEach((file) => {
+            
+            const unirRouter = `${readDirectory}\\${file}`
+            //console.log('rutas unidas', unirRouter);
+      
+            //unir rutas 
+            //luego recorro cada ruta le aplico si es directorio o si es archivo.md 
+      
+          })
+        } else {
+          console.log(`${router}`);
+        }
+      })
+      .catch((error) => console.log(error))
+  };
+  if (directoryPath) {
+    
+    
+  }
+  })
+
+  
+};
+
+mdLinks(path)
+.then((res) => res)
+.catch((err) => err)
 
