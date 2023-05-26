be050a8037a02ef45d934b5e3a60b94b72fa8b7a
fperez15 <francis.perez1590@gmail.com>
2023-05-26 00:38:24 -0500

    fix configure test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,13 +1,12 @@
-import {api} from '../api.js'
+import { existPath } from '../api.js';
 
 describe('existPath', () => {
   it('should return true if the path exist', () => {
-    expect(api.existPath('C:\Users\Francis\OneDrive\Escritorio\Laboratoria\DEV004-md-links')).toBe(true)
+    expect(existPath('C:\\Users\\Francis\\OneDrive\\Escritorio\\Laboratoria\\DEV004-md-links')).toBe(true)
   });
   it('should return false if the path does not exist', () => {
-    expect(api.existPath('C:\Users\Francis\OneDrive\Escritorio\Laboratoria\DEV04')).toBe(false)
+    expect(existPath('C:\\Users\\Francis\\OneDrive\\Escritorio\\Laboratoria\\DEV04')).toBe(false)
   });
 });
-
 
 
